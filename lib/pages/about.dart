eb29e3169abfb56ef9f75022fcdeee5b7d42617b
Ulugbek Xamidov <230035@newuzbekistanuniversity.uz>
2025-11-23 12:27:15 +0500

    made the appBar a public util function so other pages can have the same appbar

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_budget/util/button-widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smart_budget/util/appBar.dart';
 
 class AboutPage extends StatefulWidget {
   @override
@@ -11,7 +12,7 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
+      appBar: appBar(),
       backgroundColor: Colors.white,
       body: ListView(
         children: [
@@ -36,70 +37,6 @@ class _AboutPageState extends State<AboutPage> {
             )
           )
         );
-  }
-
-  AppBar _appBar() {
-    return AppBar(
-      elevation: 5,
-      shadowColor: Colors.black.withOpacity(0.5),
-      toolbarHeight: 100,           
-      titleSpacing: 0,          
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start, 
-          children: [
-            // LOGO ON THE LEFT
-            SvgPicture.asset(
-              'assets/icons/smart_budget_logo.svg',
-              width: 60,   
-              height: 60,  
-            ),
-
-            const SizedBox(width: 40),
-
-            // MENU ITEMS (left side)
-            GestureDetector(
-              onTap: () {},
-              child: const Text('Mission'),
-            ),
-            const SizedBox(width: 16),
-            GestureDetector(
-              onTap: () {},
-              child: const Text('App Info'),
-            ),
-            const SizedBox(width: 16),
-            GestureDetector(
-              onTap: () {},
-              child: const Text('Community'),
-            ),
-            const SizedBox(width: 16),
-            GestureDetector(
-              onTap: () {},
-              child: const Text('Contact Us'),
-            ),
-
-            // BUTTONS FAR RIGHT
-            const Spacer(),
-
-            Row(
-              children: [
-                CustomButton(
-                  buttonText: 'Sign in',
-                  onPressed: () {},
-                ),
-                const SizedBox(width: 10),
-                CustomButton(
-                  buttonText: 'Log in',
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
 
